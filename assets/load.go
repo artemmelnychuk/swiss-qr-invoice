53853af7bc1aecdeaea5cd1741363cb4843e726f
Artem Melnychuk <melnychuk.letter@gmail.com>
2024-12-16 13:33:47 +0100

    removed go rice

--- assets/load.go
+++ assets/load.go
@@ -1,38 +1,17 @@
 package assets
 
 import (
-	"fmt"
-
-	rice "github.com/GeertJohan/go.rice"
+	"embed"
 )
 
-func loadFromRice(filename, description string) ([]byte, error) {
-	box, err := riceBox()
-	if err != nil {
-		return nil, err
-	}
-	data, err := box.Bytes(filename)
-	if err != nil {
-		return nil, fmt.Errorf("could not load %s: %s", description, err)
-	}
-	return data, nil
-
-}
-
-func riceBox() (*rice.Box, error) {
-	box, err := rice.FindBox("raw")
-	if err != nil {
-		return nil, fmt.Errorf("rice find box failed: %s", err)
-	}
-	return box, nil
-}
+var fs embed.FS
 
 // Scissors returns the scissors image as bytes.
 func Scissors() ([]byte, error) {
-	return loadFromRice("scissors.png", "scissors image")
+	return fs.ReadFile("raw/scissors.png")
 }
 
 // CHCross returns the CH-cross image as bytes.
 func CHCross() ([]byte, error) {
-	return loadFromRice("ch-cross.png", "ch-cross image")
+	return fs.ReadFile("raw/ch-cross.png")
 }
